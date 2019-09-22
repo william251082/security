8a99468fdbdc46b67ed22f5bdfcbfd0b7bae35f5
william <william.del.rosario@iceshop.nl>
2019-09-22 21:44:54 +0200

    Serializer API

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -18,11 +18,24 @@ class AccountController extends BaseController
      */
     public function index(LoggerInterface $logger)
     {
-//        dd($this->getUser()); // will give the user object
         $logger->debug('Checking account page for '.$this->getUser()->getEmail());
 
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
         ]);
     }
+
+    /**
+     * Will return a json representation of whoever is logged in
+     * @Route("/api/account", name="api_account")
+     */
+    public function accountApi()
+    {
+        $user = $this->getUser();
+
+        return $this->json($user, 200, [], [
+            'groups' => ['main']
+        ]);
+    }
+
 }
