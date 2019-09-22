6b6d083e0e2c13885850290cf3aaff8a0b106640
william <william.del.rosario@iceshop.nl>
2019-09-22 22:05:13 +0200

    Api token entity

---
+++ src/Entity/ApiToken.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\This;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ApiTokenRepository")
+ */
+class ApiToken
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $expiresAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="apiTokens")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    public function __construct(User $user)
+    {
+        // token is now immutable
+        $this->token = bin2hex(random_bytes(60));
+        $this->expiresAt = new \DateTime('+ 1 hour');
+        $this->user = $user;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function getExpiresAt(): ?\DateTimeInterface
+    {
+        return $this->expiresAt;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+}
