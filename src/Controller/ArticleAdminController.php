74c5f9faa460332f1b357a92efdea030b6a30adb
william <william.del.rosario@iceshop.nl>
2019-09-24 06:55:19 +0200

    Article admin setup

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -5,21 +5,20 @@ namespace App\Controller;
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class ArticleAdminController
  * @package App\Controller
- * @IsGranted("ROLE_ADMIN_ARTICLE")
  */
 class ArticleAdminController extends BaseController
 {
     /**
      * @Route("/admin/article/new", name="admin_article_new")
+     * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $manager)
     {
         die('todo');
 
@@ -28,5 +27,18 @@ class ArticleAdminController extends BaseController
             $article->getId(),
             $article->getSlug()
         ));
+    }
+
+    /**
+     * @Route("/admin/article/{id}/edit")
+     */
+    public function edit(Article $article)
+    {
+        // Never duplicate security logic, make this reusable -- solution: voters
+        if ($article->getAuthor() != $this->getUser() && !$this->isGranted('ROLE_ADMIN_ARTICLE')) {
+            throw $this->createAccessDeniedException('No access!');
+        }
+
+        dd($article);
     }
 }
